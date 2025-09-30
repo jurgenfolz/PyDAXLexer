4b7ff332713a4ec3b23f342ab38449be8f68db63
KlausFolzBrunner <klausjurgenfolz@gmail.com>
2025-09-30 09:56:35 +0200

    Update antlr4-python3-runtime to version 4.13.2 and regenerate lexer files

--- src/PyDAX/PyDAXLexer.py
+++ src/PyDAX/PyDAXLexer.py
@@ -1,4 +1,4 @@
-# Generated from src/PyDAX/PyDAXLexer.g4 by ANTLR 4.10.1
+# Generated from src/PyDAX/PyDAXLexer.g4 by ANTLR 4.13.2
 from antlr4 import *
 from io import StringIO
 import sys
@@ -2692,7 +2692,7 @@ class PyDAXLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.10.1")
+        self.checkVersion("4.13.2")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
